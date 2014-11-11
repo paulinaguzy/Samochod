6cea32413b6be77504e6b20c9d103d0d417951d8
Paulina Guzy <s173566@v-ie.uek.krakow.pl>
2014-11-11 21:29:06 +0100

    Add file SamochodWyscigowyInterface.php

---
+++ SamochodWyscigowyInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+interface SamochodWyscigowyInterface
+{
+    public function getPredkoscMaksymalna();
+	public function setPredkoscMaksymalna($predkoscMaksymalna);
+    public function getPrzyspieszenie();
+	public function setPrzyspieszenie($przyspieszenie);
+}
+
+?>
